9ffcc31e8de025d7ff0bf86250e13c8b56f27ae9
Hans <hans@Hans-Mac.local>
2021-10-13 21:06:35 +0900

    redirect uri editted

--- src/constants/kakao-auth.js
+++ src/constants/kakao-auth.js
@@ -2,4 +2,4 @@ export const BASE_URI='https://kauth.kakao.com/oauth/authorize?';
 export const CLIENT_ID='e343c9f82222cc6cc84c721b9e869b3c';
 export const SECRET_KEY='dRmnbLf8JJClsU7NjPWEkjKLTb4F1T6K';
 export const CALLBACK_URI='http://ec2-18-221-128-31.us-east-2.compute.amazonaws.com/auths/kakao/callback';
-export const GETTOKEN_URI='http://ec2-18-221-128-31.us-east-2.compute.amazonaws.com/api/auths/kakao';
+export const GETTOKEN_URI='http://ec2-3-17-139-14.us-east-2.compute.amazonaws.com/api/auths/kakao';
